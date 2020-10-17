efe7e2530dd1e9688b8f4de53d218343de3dee4f
fabianrp7 <65554281+fabianrp7@users.noreply.github.com>
2020-10-17 13:03:16 -0500

    gorilla test

--- vars/deployJava.groovy
+++ vars/deployJava.groovy
@@ -3,18 +3,18 @@ def call(project) {
     def cmnStgs = new com.devops.jenkins.cmn.CmnStgs(this)
     def javaStgs = new com.devops.jenkins.java.JavaStgs(this)
     node(label:project.node){
-        cmnStgs.stgCheckoutSCM(project)  
-        javaStgs.stgBuild()
-        if(project.stgUnitTests){
-            javaStgs.stgTest()
-        }        
-        javaStgs.stgSonarQubeAnalysis(project)
-        if(project.stgSQualityGate){
-            cmnStgs.stgSonarQubeQGate(project)
-        }
-        if(project.stgKiuwan){
-            cmnStgs.stgKiuwanAnalysis(project) 
-        }                 
-        cmnStgs.stgDeployUrbanCodeDeploy(project)
+        // cmnStgs.stgCheckoutSCM(project)  
+        // javaStgs.stgBuild()
+        // if(project.stgUnitTests){
+        //     javaStgs.stgTest()
+        // }        
+        // javaStgs.stgSonarQubeAnalysis(project)
+        // if(project.stgSQualityGate){
+        //     cmnStgs.stgSonarQubeQGate(project)
+        // }
+        // if(project.stgKiuwan){
+        //     cmnStgs.stgKiuwanAnalysis(project) 
+        // }                 
+        // cmnStgs.stgDeployUrbanCodeDeploy(project)
     }
 }
